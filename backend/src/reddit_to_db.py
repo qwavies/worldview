9635a109fd1a8c3105580955d805e63a50216bac
Josh-Loke_Origin <josh.loke@origin.com.au>
2026-03-14 15:02:18 +1100

    Create reddit_to_db.py

---
+++ backend/src/reddit_to_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+from datetime import datetime
+
+def save_to_db(countryA, countryB, avg_score):
+    conn = sqlite3.connect('sentiment_data.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS sentiment_results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            country_a TEXT,
+            country_b TEXT,
+            score REAL,
+            date_created DATETIME
+        )
+    ''')
+
+    cursor.execute('''
+        INSERT INTO sentiment_results (country_a, country_b, score, date_created)
+        VALUES (?, ?, ?, ?)
+    ''', (countryA, countryB, avg_score, datetime.now()))
+
+    conn.commit()
+    conn.close()
