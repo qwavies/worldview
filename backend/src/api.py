231eaed4dc6fb4d17ab14963032af22f2676e55a
qwavies <qwavsbusiness@gmail.com>
2026-03-14 15:25:29 +1100

    call reddit scraper in `api.py`

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from src.pydantic_models import CountryABQuery
+from src.RedditScrapper import RedditScrapper
 
 app = FastAPI()
 
@@ -9,4 +10,6 @@ def test_api():
 
 @app.get("/sentiment")
 def get_countryab_sentiment(countryAB: CountryABQuery):
-    return {"a_to_b":0.2, "b_to_a":0.5}
+    a_to_b = RedditScrapper(countryAB.countryA, countryAB.countryB)
+    b_to_a = RedditScrapper(countryAB.countryB, countryAB.countryA)
+    return {"a_to_b":a_to_b, "b_to_a":b_to_a}
